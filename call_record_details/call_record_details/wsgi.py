ea7165451f5bd29fc54e28db69dceaad975a9d14
igorsbrito <igorsbrito93@gmail.com>
2018-07-09 22:19:46 -0300

    setting up the project

---
+++ call_record_details/call_record_details/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for call_record_details project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "call_record_details.settings")
+
+application = get_wsgi_application()
