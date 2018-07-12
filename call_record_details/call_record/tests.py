a8a8bca61276862e08d3cff718aee14963139776
igorsbrito <igorsbrito93@gmail.com>
2018-07-12 20:47:16 -0300

    test of the creation of call_record

--- call_record_details/call_record/tests.py
+++ call_record_details/call_record/tests.py
@@ -10,9 +10,9 @@ class RecordTestCase(TestCase):
         Record_model.objects.create(
             type=0,
             call_id=70,
-            time="2017-12-12T15:07:13Z",
-            origin_phone_number="99988526423",
-            destination_phone_number="9993468278"
+            timestamp="2017-12-12T15:07:13Z",
+            source="99988526423",
+            destination="9993468278"
         )
 
 
@@ -20,7 +20,21 @@ class RecordTestCase(TestCase):
         start = Record_model.objects.get(call_id=70,type=0)
         date = datetime.strptime("2017-12-12T15:07:13Z", "%Y-%m-%dT%H:%M:%SZ")
 
-        self.assertEqual(start.time.strftime("%Y-%m-%d %H:%M:%S"),date.strftime("%Y-%m-%d %H:%M:%S"))
+        self.assertEqual(start.timestamp.strftime("%Y-%m-%d %H:%M:%S"),date.strftime("%Y-%m-%d %H:%M:%S"))
         self.assertEqual(start.call_id, 70)
-        self.assertEqual(start.origin_phone_number,"99988526423")
-        self.assertEqual(start.destination_phone_number,"9993468278")
+        self.assertEqual(start.source,"99988526423")
+        self.assertEqual(start.destination,"9993468278")
+
+    def teste_call_record_create(self):
+        data ={
+            'call_id':70,
+            'type':1,
+            'timestamp':'2016-02-29T14:00:00Z',
+        }
+
+        response = self.client.post('/record/record_call/', data,follow=True)
+        self.assertEqual(response.status_code,201)
+
+        #This second insertion should not create a new call_record because already exist in the database one call_record with this call id and this type.
+        response = self.client.post('/record/record_call/', data, follow=True)
+        self.assertEqual(response.status_code, 400)
