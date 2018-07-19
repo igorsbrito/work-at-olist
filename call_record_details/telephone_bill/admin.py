1cde4f5fbcfc3dd7f9a5b0996112f4cb3a847764
igorsbrito <igorsbrito93@gmail.com>
2018-07-18 23:51:11 -0300

    update admin

--- call_record_details/telephone_bill/admin.py
+++ call_record_details/telephone_bill/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Telephone_bill_call_model
 
 class Telephone_bill_call_admin(admin.ModelAdmin):
-    list_display = ['call_id', 'source','call_duration','call_price']
+    list_display = ['call_id', 'source','call_price']
 
 admin.site.register(Telephone_bill_call_model, Telephone_bill_call_admin)
